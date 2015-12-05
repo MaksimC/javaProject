6637cdcb99de6d11a1c5890c8584642cc533e835
Maksim Tseljabov <maksim.tseljabov@ericsson.com>
2015-12-05 23:38:57 +0200

    update

--- src/MemoryTest/main.java
+++ src/MemoryTest/main.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.StrokeLineCap;
 import javafx.stage.Stage;
@@ -16,7 +18,7 @@ import javafx.stage.Stage;
  */
 public class main extends Application {
     Stage window1;
-    Scene scene1, scene2;
+    Scene scene1, scene2, scene3;
 
     public static void main () throws Exception {
         // define which methods will be used in program
@@ -52,18 +54,6 @@ public class main extends Application {
         layout1.setAlignment(Pos.CENTER);
         scene1 = new Scene (layout1, 800, 600);
 
-        // Scene 2 elements
-        Label label2 = new Label("Press start to begin test");
-        Button buttonStart = new Button("start");
-        buttonStart.setOnAction(e -> imageData.array());
-        Label label3 = new Label(ageInput.getText());
-
-        //Scene 2 layout
-        VBox layout2 = new VBox(30);
-        layout2.getChildren().addAll(label2, buttonStart, label3);
-        layout2.setAlignment(Pos.CENTER);
-        scene2 = new Scene(layout2,800, 600);
-
         window1.setScene(scene1);
         window1.setTitle("Memory test");
 
@@ -84,7 +74,8 @@ public class main extends Application {
         try {
             int age = Integer.parseInt(inputAge);
             System.out.println("Name is " + inputName + ", Age is " + age);
-            window1.setScene(scene2);
+            gameStart();
+
             return true;
         } catch (NumberFormatException e) {
             errorPopUp.errorPop("Title", "Message");
@@ -93,5 +84,46 @@ public class main extends Application {
 
     }
 
+    public void gameStart() {
+        // Scene 2 elements
+        Label label2 = new Label("Press start to begin test");
+        Button buttonStart = new Button("start");
+        buttonStart.setOnAction(e -> array());
+        /* Label label3 = new Label(ageInput.getText());*/
+
+        //Scene 2 layout
+        VBox layout2 = new VBox(30);
+        layout2.getChildren().addAll(label2, buttonStart);
+        layout2.setAlignment(Pos.CENTER);
+        scene2 = new Scene(layout2,800, 600);
+        window1.setScene(scene2);
+
+        window1.setScene(scene2);
+        window1.setTitle("Memory Game");
+        window1.show();
+    }
+    public void array() {
+        Image[] allImages = new Image[49];
+        String filelocation = new String();
+
+        for (int i = 0; i < 49; i++) {
+
+            filelocation = "C:\\Users\\emaktse\\Documents\\HITSA\\GIT Repository\\javaProject\\Images library\\" + i + ".gif";
+            allImages[i] = new Image("file:"+filelocation);
+        }
+
+        //scene3 elements
+        ImageView Picture1 = new ImageView();
+        Picture1.setImage(allImages[48]);
+
+        //Scene 3 layout
+        BorderPane layout3 = new BorderPane();
+        layout3.setCenter(Picture1);
+        scene3 = new Scene(layout3, 800, 600);
+
+        window1.setScene(scene3);
+        window1.show();
+
+    }
 }
 
